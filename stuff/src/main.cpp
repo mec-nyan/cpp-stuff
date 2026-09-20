9f71d8740a08efc1ca452eed4fdcd979c9304c1d
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-09-19 21:03:29 -0300

    [modularise] separate mutability example.

--- stuff/src/main.cpp
+++ stuff/src/main.cpp
@@ -7,7 +7,7 @@
 #include "area.hpp"
 #include "hello.hpp"
 #include "initialisation.hpp"
-#include "point.hpp"
+#include "mutability.hpp"
 
 using namespace std;
 
@@ -17,34 +17,11 @@ int main ()
     // Call a package's functions to see if they're working and compiling properly.
     initialisation();
 
-    /////////////////
-    // Mutability. //
-    /////////////////
+    // Mutability
+    mutability();
 
-    // These are mutable:
-    auto x{ 0 };
-    auto y{ 1 };
-
-    // This is alright:
-    x = 42;               // BTW this is called assignment iydk.
-    print_point ( x, y ); // "Point at (42, 1)"
-
-    // You can create constants in (at least) two ways:
-    {
-        // "I promise I won't change it" style.
-        const auto x{ 17 };
-        // "Compile time" enforced style (use this whenever possible).
-        constexpr auto y{ 33 };
-
-        // x = 42;  Wrong! You cannot change this binding!
-        // y = 123;  Wrong! You cannot change this binding!
-
-        print_point ( x, y );
-    }
-
-    // [#include] can still be used:
-
-    x = 21;
+    // Use of "#include"d function/symbol:
+    auto x = 21;
     println ( "{} + {} is {}.", x, x, sum_some ( x, x ) );
 
     return 0;
---
+++ stuff/src/mutability.cpp
@@ -0,0 +1,30 @@
+#include "mutability.hpp"
+#include "point.hpp"
+
+/////////////////
+// Mutability. //
+/////////////////
+
+auto mutability () -> void
+{
+    // These are mutable:
+    auto x{ 0 };
+    auto y{ 1 };
+
+    // This is alright:
+    x = 42;               // BTW this is called assignment iydk.
+    print_point ( x, y ); // "Point at (42, 1)"
+
+    // You can create constants in (at least) two ways:
+    {
+        // "I promise I won't change it" style.
+        const auto x{ 17 };
+        // "Compile time" enforced style (use this whenever possible).
+        constexpr auto y{ 33 };
+
+        // x = 42;  Wrong! You cannot change this binding!
+        // y = 123;  Wrong! You cannot change this binding!
+
+        print_point ( x, y );
+    }
+}
