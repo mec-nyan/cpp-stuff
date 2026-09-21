805511b1de03c81b58e31e13f8806781d6a96e69
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-09-21 20:32:06 -0300

    Improve initialisation examples.

--- stuff/src/initialisation.cpp
+++ stuff/src/initialisation.cpp
@@ -1,5 +1,6 @@
 #include "initialisation.hpp"
 #include "area.hpp"
+#include <cstdint>
 #include <print>
 #include <string>
 using namespace std;
@@ -12,24 +13,42 @@ using namespace std;
 // Prefer the universal, list initialisation (x = {...}).
 auto initialisation () -> void
 {
-
-    // Traditional (Inherited from C. Allows narrowing conversions):
-    string me = "I";
+    // PREFERRED //
     // Universal "list" initialisation (prefer this one when possible):
+    // The basic form is `type name { initialiser }`.
     string love{ "💖" };
+
     // "=" is optional in the list initialisation (Don't use it...).
     string cpp = { "C++" };
+
+    {
+        // NOTEs:
+        // You can use `auto` to let the compiler deduce the variable's type.
+        // Be aware that sometimes it won't be what you expect ...
+        auto foo{ "foo" };            // Not a string! The deduced type will be 'const char*' ...
+        auto bar{ string ( "bar" ) }; // OK, but what's the point.  You had to specify the type anyway.
+        auto n{ 42 };                 // OK if you want an 'int'.
+        int32_t m{ 42 };              // Otherwise, be specific with the type you need.
+
+        println ( "{}, {}, {}, {}", foo, bar, n, m );
+    }
+
+    // Traditional (inherited from C, allows narrowing conversions) can also be used:
+    string me = "I";
+
     println ( "{} {} {}!", me, love, cpp );
 
-    // [initialisation]
-    // int rad = 12.5; // Oops! rad is now "12" (probably not what you wanted).
-    auto rad{ 12.5 }; // Use list initialisation instead.
+    // Beware of traditional initialisation ...
+    int bad_rad = 12.5; // Will compile, but now 'bad_rad' is '12' ...
+    auto rad{ 12.5 };   // Use list initialisation instead.
     auto area{ get_circle_area ( rad ) };
 
     println ( "The area of a circle of radius {} is {:.2f}.", rad, area );
 
     // [scope/lifetime]
     {
+        // Each block defines a new lexical scope.
+        //
         // 'side' starts its life here.
         auto side{ 10 };
         // NOT the same 'area' than before!
